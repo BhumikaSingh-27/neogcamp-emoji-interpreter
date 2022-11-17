ad8cb819a7375500796efb9c51ad58c7e74bbde6
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2022-11-18 00:51:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import "./styles.css";
+import { useState } from "react";
+
+const emojiDictionary = {
+  "😊": "similing",
+  "😁 ": "gratified happiness",
+  "😒": "irritation",
+  "😴": "sleeping ",
+  "😍": "adorable",
+  "😘": "kissing",
+  "👍": "approval ",
+  "🫤": "frustation",
+  "😑": "annoyance",
+  "🥱": "mid-yawn",
+  "🤣": "hysterical laughter",
+  "🥰": "affectionate",
+  "🤗": "hug",
+  "😶‍🌫️": "confusion",
+  "🫡": "saluting",
+  "❤️": "love",
+  "🤤": "delicious"
+};
+
+const freuentlyUsedEmojiDict = {
+  "😍": "adorable",
+  "😊": "similing",
+  "😒": "irritation",
+  "😴": "sleeping ",
+  "❤️": "love",
+  "🤤": "delicious",
+  "😑": "annoyance",
+  "🥱": "mid-yawn",
+  "🤣": "hysterical laughter"
+};
+
+var frequestEmoji = Object.keys(freuentlyUsedEmojiDict);
+
+export default function App() {
+  var [meaning, setEmojiMeaning] = useState("");
+
+  function emojiHandler(event) {
+    var emojiInput = event.target.value;
+    var meaning = emojiDictionary[emojiInput];
+    if (meaning === undefined) {
+      meaning = "failure to recognize emoji!";
+    }
+    setEmojiMeaning(meaning);
+  }
+
+  function clickEmojiHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setEmojiMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Emoji Interpreter - Express your emotions right!</h1>
+      <input
+        onChange={emojiHandler}
+        placeholder="Enter emoji to know the meaning"
+      />
+      <div style={{ fontSize: "2rem", padding: "1rem" }}>{meaning}</div>
+
+      <h3 style={{ marinTop: "0px" }}> Frequently used..</h3>
+
+      {frequestEmoji.map(function (emoji) {
+        return (
+          <span
+            key={emoji}
+            onClick={() => clickEmojiHandler(emoji)}
+            style={{ cursor: "pointer", fontSize: "2rem" }}
+          >
+            {" "}
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
